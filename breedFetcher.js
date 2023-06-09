cd36fb6fd02a6315fcf691e179c2512781a183c7
adavidliang <adavidliang@gmail.com>
2023-06-09 15:38:10 -0400

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,30 @@
 const request = require('request');
 
-const read = process.argv.slice(2);
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${read}`;
-request(url, (error, response, body) => {
-  // Request Failed;
-  if (error) {
-    console.log('request error', error);
-    return;
-  }
 
-  // Breed Not Found;
-  const data = JSON.parse(body);
-  data[0] === undefined
-    ? console.log("Breed Not Found")
-    : console.log(data[0].description);
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    // console.log(breedName);
+    //Request Failed;
+    if (error) {
+      callback(error, null);
+      return;
+    }
+  
+    //Breed Not Found;
+    const data = JSON.parse(body);
+    // console.log(data);
+    // console.log(data[0]);
+    if (data[0] === undefined) {
+      return (callback("Breed Not Found", null));
+    } else {
+      return callback(null, data[0].description);
+    }
 
-});
+  
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
